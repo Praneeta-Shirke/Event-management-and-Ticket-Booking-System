4e3286333630681238a1658cb610ff1ec7a56248
Praneeta-Shirke <praneetashirke30@gmail.com>
2025-12-26 00:19:48 +0530

    feat: User Repository feature added

---
+++ src/main/java/com/smart/event/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.smart.event.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.smart.event.event_management_system.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,long> {
+    List<User> findAll(); 
+}
