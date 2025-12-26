8f307492024a077a1ade9b0f7bd02f5517aa5e83
Praneeta-Shirke <praneetashirke30@gmail.com>
2025-12-26 12:32:55 +0530

    feat: Event Services Interface added

---
+++ src/main/java/com/smart/event/event_management_system/service/EventService.java
@@ -0,0 +1,15 @@
+package com.smart.event.event_management_system.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.smart.event.event_management_system.entity.Event;
+
+public interface EventService {
+    Event createEvent(Event event, Long organizerId);
+    List<Event> getAllEvents();
+    List<Event> getActiveEvents();
+    List<Event> getEventsByDate(LocalDate date);
+    Event getEventById(Integer eventId);
+    void cancelEvent(Integer eventId);
+}
