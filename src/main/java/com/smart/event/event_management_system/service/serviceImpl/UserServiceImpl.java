ac5e7973d3055628d916df28c582e442ba07040b
Praneeta-Shirke <praneetashirke30@gmail.com>
2025-12-26 13:07:45 +0530

    fix: User service Bugs fixed.

--- src/main/java/com/smart/event/event_management_system/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/smart/event/event_management_system/service/serviceImpl/UserServiceImpl.java
@@ -3,10 +3,13 @@ package com.smart.event.event_management_system.service.serviceImpl;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.smart.event.event_management_system.entity.User;
 import com.smart.event.event_management_system.repository.UserRepository;
 import com.smart.event.event_management_system.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
