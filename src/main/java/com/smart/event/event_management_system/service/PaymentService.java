93969ece5ad4bd95aa6294b996cdf15c3b8a4f79
Praneeta-Shirke <praneetashirke30@gmail.com>
2025-12-26 12:35:53 +0530

    feat : Payment Service Interface added.

---
+++ src/main/java/com/smart/event/event_management_system/service/PaymentService.java
@@ -0,0 +1,7 @@
+package com.smart.event.event_management_system.service;
+
+import com.smart.event.event_management_system.entity.Payment;
+
+public interface PaymentService {
+    Payment makePayment(Integer bookingId, String mode);
+}
