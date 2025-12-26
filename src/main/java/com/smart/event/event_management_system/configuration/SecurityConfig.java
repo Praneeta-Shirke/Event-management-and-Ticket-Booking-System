04e922e009ed75d2314f123d067cc2324f144571
Praneeta-Shirke <praneetashirke30@gmail.com>
2025-12-26 16:44:51 +0530

    Feat: Security configuration changed to handle API call's Authentication Error.

---
+++ src/main/java/com/smart/event/event_management_system/configuration/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.smart.event.event_management_system.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())   // REQUIRED for POST from Postman
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
